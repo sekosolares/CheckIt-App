26f082f6ab76776e3a0c1f718afff5802e46df0e
sekosolares <axel.solares.g@gmail.com>
2022-03-25 14:58:53 -0600

    Composicion de Componentes ejemplo

--- src/App/index.js
+++ src/App/index.js
@@ -4,11 +4,56 @@ import { AppUI } from './AppUI';
 
 
 function App() {
+  const [state, setState] = React.useState('Estado compartido');
+
   return (
-    <TodoProvider>
-      <AppUI/>
-    </TodoProvider>
+      <React.Fragment>
+        <TodoHeader>
+          <TodoCounter />
+          <TodoSearch />
+        </TodoHeader>
+        <TodoList>
+          <TodoItem state={state} />
+        </TodoList>
+      </React.Fragment>
+  )
+}
+
+function TodoHeader({ children }) {
+  return (
+    <header>
+      {children}
+    </header>
   );
 }
+
+function TodoList({ children }) {
+  return (
+    <ul>
+      {children}
+    </ul>
+  );
+}
+
+function TodoCounter() {
+  return <p>Counter</p>
+}
+
+function TodoSearch() {
+  return <p>Search</p>
+}
+
+function TodoItem({ state }) {
+  return <p>Item {state}</p>
+}
+
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI/>
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -4,16 +4,16 @@ import './TodoItem.css';
 function TodoItem(props) {
       return (
             <li className="TodoItem">
+                  <span
+                        className="TodoItem-Delete"
+                        onClick={props.onDelete}>&times;</span>
+                  <p className={`TodoItem-Text ${props.completed && 'Text-Completed'}`}> {props.text} </p>
                   <input
                         type="checkbox"
                         defaultChecked={props.completed}
                         className={`TodoItem-CheckBox ${props.completed ? 'CheckBox-Completed' : 'CheckBox-Pending'}`}
                         onClick={props.completed ? props.onUnComplete : props.onComplete}
                   />
-                  <p className={`TodoItem-Text ${props.completed && 'Text-Completed'}`}> {props.text} </p>
-                  <span
-                        className="TodoItem-Delete"
-                        onClick={props.onDelete}>&times;</span>
             </li>
       );
 }
