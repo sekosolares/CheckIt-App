177f8f32620df45c9ab46eb00de4542b6ff42bbf
sekosolares <axel.solares.g@gmail.com>
2022-03-27 14:38:37 -0600

    High Order Components example code on App component.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
+// import App from './App';
+
+
+function App(props) {
+  return (
+    <h1>¡{props.saludo}, {props.nombre}!</h1>
+  )
+}
+
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComonentWithSaludo(saludo) {
+    return function ComponenteDeverdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      )
+    }
+  }
+}
+
+// function withWhatever(WrappedComponent) {
+//   return function ComponenteDeverdad(props) {
+//     return (
+//       <React.Fragment>
+//         <WrappedComponent {...props} />
+//         <p>Estamos acompañando al WrappedComponent</p>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const AppWithSaludo = withSaludo(App)("Aloha");
 
 ReactDOM.render(
-  <App/>,
+  <AppWithSaludo
+    nombre="Juanita"
+  />,
+  // <App
+  //   saludo="Buenas" nombre="Pepe"
+  // />,
   document.getElementById('root')
 );
-
-
-// ReactDOM.createPortal(
-//   <App/>,
-//   document.getElementById('modal')
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
